e0d085a6637948103a543ba5a446a90b1d255740
pro100serj <kinbin@mail.ru>
2024-02-10 15:55:42 +0700

    Add axios

--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+createApp(App).use(router).use(VueAxios, axios).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="getList">Get list</button>
   </div>
 </template>
 
@@ -9,10 +10,20 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+const url = 'https://newsite.softorium.pro/admin-area/api/visitor/vacancy/';
+
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+
+  methods: {
+    getList() {
+      this.axios.get(url).then((response) => {
+        console.log(response.data)
+      })
+    }
   }
 }
 </script>
